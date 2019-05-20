02daad104c1ef2f0c869e4157b1e7946475db10f
Nelson Perez <elnelsonperez@gmail.com>
2019-05-20 12:39:27 -0400

    Done!

--- src/features/tv-list/components/TvShowCard.tsx
+++ src/features/tv-list/components/TvShowCard.tsx
@@ -13,10 +13,7 @@ export const TvShowCard: React.FC<Props> = props => {
         <ConfigContext.Consumer>
             { (config) => config ? <Card interactive={true} elevation={Elevation.ONE}>
                 <h5>{tvShow.name} - Rating: {tvShow.vote_average} <Icon icon={'clean'} color={'#ff6c00'}/> </h5>
-                {/*<p>{tvShow.overview}</p>*/}
                 <img src={config.images.base_url+'w185/'+tvShow.poster_path} alt={'Poster'}/>
-                {/*<p>Card content</p>*/}
-                {/*<Button>Submit</Button>*/}
             </Card> : false
             }
         </ConfigContext.Consumer>
