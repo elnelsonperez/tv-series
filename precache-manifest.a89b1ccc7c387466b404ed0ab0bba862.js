72a28fc4205e0db5f0f844c24753c89a8c3362f5
Nelson Perez <elnelsonperez@gmail.com>
2019-08-28 15:11:21 -0400

    Updates

---
+++ precache-manifest.a89b1ccc7c387466b404ed0ab0bba862.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eec8ec249119e0fd87d2eaf0d03a07",
+    "url": "/tv-series/index.html"
+  },
+  {
+    "revision": "8229aeb24c513eb8f9fc",
+    "url": "/tv-series/static/css/2.e599c9ff.chunk.css"
+  },
+  {
+    "revision": "313d64174894702186ae",
+    "url": "/tv-series/static/css/main.51ca4b5f.chunk.css"
+  },
+  {
+    "revision": "8229aeb24c513eb8f9fc",
+    "url": "/tv-series/static/js/2.a72c7d1e.chunk.js"
+  },
+  {
+    "revision": "313d64174894702186ae",
+    "url": "/tv-series/static/js/main.bd273953.chunk.js"
+  },
+  {
+    "revision": "b7f4a2e8423d01f88544",
+    "url": "/tv-series/static/js/runtime~main.466b3e8c.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/tv-series/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/tv-series/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/tv-series/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/tv-series/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/tv-series/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/tv-series/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/tv-series/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/tv-series/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/tv-series/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/tv-series/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/tv-series/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/tv-series/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/tv-series/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/tv-series/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/tv-series/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "3326c360137c2d6a6909d0c4303e502a",
+    "url": "/tv-series/static/media/icons-16.3326c360.eot"
+  },
+  {
+    "revision": "895bc7de59413bb8029ac83cac8c9e1b",
+    "url": "/tv-series/static/media/icons-16.895bc7de.woff"
+  },
+  {
+    "revision": "b6382c6d9ffca4a1c50f58a17fce15a9",
+    "url": "/tv-series/static/media/icons-16.b6382c6d.ttf"
+  },
+  {
+    "revision": "058e48cbbee6cdbfb2a2e6dab347f245",
+    "url": "/tv-series/static/media/icons-20.058e48cb.woff"
+  },
+  {
+    "revision": "aa8f3ca044cd237f87b69d0cb364887a",
+    "url": "/tv-series/static/media/icons-20.aa8f3ca0.ttf"
+  },
+  {
+    "revision": "e666d692a7dc07ea69886dfcec837c3c",
+    "url": "/tv-series/static/media/icons-20.e666d692.eot"
+  }
+]);
